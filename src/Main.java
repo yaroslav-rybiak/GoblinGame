a299dd2dc4ce2630f12b5a9f9670b1ab599d5146
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2016-10-11 14:07:01 +0300

    creation of new player

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.io.IOException;
+
 public class Main {
 
+    public static void main(String[] args) throws IOException {
 
+        Player player = new Player(Player.askName());
+        player.getInfo();
+    }
 
 }
--- src/Player.java
+++ src/Player.java
@@ -1,9 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Player {
+
+    public static String askName() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Please tell me your name");
+        return br.readLine();
+    }
 
     private String name;
     private int level;
     private int health;
     private int strength;
+
+    public void getInfo() {
+        System.out.println(String.format("Your name is %s, your level is %d. You have %d health and %d strength.",
+                this.name, this.level, this.health, this.strength));
+    }
 
     public int getStrength() {
         return this.strength;
