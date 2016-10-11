b33944d4488b09da7761459255f8a17f28776432
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2016-10-11 14:42:06 +0300

    monster constructor

--- src/Monster.java
+++ src/Monster.java
@@ -1,11 +1,18 @@
 public class Monster {
 
     private int strength;
+    private String type;
+    private String name;
+
+    private String[] names ={"goblin", "demon", "mandrake", "dragon", "troll", "morlock", "golem", "kraken", "harpy"};
+    private String[] types ={"water", "fire", "metal", "ghost", "grass", "thermal", "sea", "lake", "greasy", "hairy"};
 
     //constructor
     Monster (Player player)
     {
         this.strength = Helper.getRandomNumber(player.getStrength()-1, player.getStrength()+1);
+        this.type = types[Helper.getRandomNumber(0, types.length - 1)];
+        this.name = names[Helper.getRandomNumber(0, names.length - 1)];
     }
 
 }
